683e7ac9dfda8a3fa89bb52329add9bfa8d82ee5
daviddppaez <w.davidpaez.g@gmail.com>
2025-07-10 17:05:20 -0500

    Correción de ejercicio 05_app.js

--- 05_app.js
+++ 05_app.js
@@ -1,7 +1,13 @@
 // Librerias necesarias
 const prompt = require("prompt-sync")();
 
-// Único ejercicio, tablas de multiplicar
+//Primer ejercicio, contar con "for"
+// Mera impresión de resultado
+for (let genCont = 1; genCont <= 10; genCont++){
+    console.log(genCont)
+}
+
+// Segundo ejercicio, tablas de multiplicar
 // Solicitud de información
 let genNum = prompt("Dime un número y te doy la tabla de multiplicar: ");
 
